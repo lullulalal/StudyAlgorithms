816fa867cdaa627e221ac7d71effd8c431ed832d
lullulalal <lullulalal@gmail.com>
2018-12-24 00:01:43 +0900

    2018/12/24
    Maximum Width Ramp

---
+++ leetcode/Maximum Width Ramp/Solution.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int maxWidthRamp(int[] A) {
+        int size = A.length;
+        int max = 0;
+
+        for(int i = 0; i < size; i++){
+            if( (size - 1 - i) <= max )
+                break;
+
+            for(int j = size - 1; j > i; j--){
+                int width = j - i;
+
+                if(width <= max) break;
+
+                if(A[i] <= A[j] && width > max){
+                    max = width;
+                    break;
+                }
+            }
+        }
+
+        return max;
+    }
+}
